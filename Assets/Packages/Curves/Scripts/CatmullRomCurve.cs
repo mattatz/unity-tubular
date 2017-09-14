0ba485f0c8e539e0eef4c6d5c2615f99343f2728
mattatz <supa.matz.naka@gmail.com>
2017-09-14 13:06:13 +0900

    Fix indentations.

--- Assets/Packages/Curves/Scripts/CatmullRomCurve.cs
+++ Assets/Packages/Curves/Scripts/CatmullRomCurve.cs
@@ -4,106 +4,106 @@ using UnityEngine;
 
 namespace Curve {
 
-	public class CubicPoly {
+    public class CubicPoly {
 
-		float c0, c1, c2, c3;
+        float c0, c1, c2, c3;
 
-		public CubicPoly() {
-		}
+        public CubicPoly() {
+        }
 
-		/*
-		 * Compute coefficients for a cubic polynomial
-		 *   p(s) = c0 + c1*s + c2*s^2 + c3*s^3
-		 * such that
-		 *   p(0) = x0, p(1) = x1
-		 *  and
-		 *   p'(0) = t0, p'(1) = t1.
-		 */
-		void Init(float x0, float x1, float t0, float t1) {
-			c0 = x0;
-			c1 = t0;
-			c2 = - 3 * x0 + 3 * x1 - 2 * t0 - t1;
-			c3 = 2 * x0 - 2 * x1 + t0 + t1;
-		}
+        /*
+         * Compute coefficients for a cubic polynomial
+         *   p(s) = c0 + c1*s + c2*s^2 + c3*s^3
+         * such that
+         *   p(0) = x0, p(1) = x1
+         *  and
+         *   p'(0) = t0, p'(1) = t1.
+         */
+        void Init(float x0, float x1, float t0, float t1) {
+            c0 = x0;
+            c1 = t0;
+            c2 = - 3 * x0 + 3 * x1 - 2 * t0 - t1;
+            c3 = 2 * x0 - 2 * x1 + t0 + t1;
+        }
 
-		public void InitCatmullRom(float x0, float x1, float x2, float x3, float tension) {
-			Init(x1, x2, tension * (x2 - x0), tension * (x3 - x1));
-		}
+        public void InitCatmullRom(float x0, float x1, float x2, float x3, float tension) {
+            Init(x1, x2, tension * (x2 - x0), tension * (x3 - x1));
+        }
 
-		public void InitNonuniformCatmullRom(float x0, float x1, float x2, float x3, float dt0, float dt1, float dt2) {
-			// compute tangents when parameterized in [t1,t2]
-			var t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1;
-			var t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2;
+        public void InitNonuniformCatmullRom(float x0, float x1, float x2, float x3, float dt0, float dt1, float dt2) {
+            // compute tangents when parameterized in [t1,t2]
+            var t1 = (x1 - x0) / dt0 - (x2 - x0) / (dt0 + dt1) + (x2 - x1) / dt1;
+            var t2 = (x2 - x1) / dt1 - (x3 - x1) / (dt1 + dt2) + (x3 - x2) / dt2;
 
-			// rescale tangents for parametrization in [0,1]
-			t1 *= dt1;
-			t2 *= dt1;
-			Init(x1, x2, t1, t2);
-		}
+            // rescale tangents for parametrization in [0,1]
+            t1 *= dt1;
+            t2 *= dt1;
+            Init(x1, x2, t1, t2);
+        }
 
-		public float Calc(float t) {
-			var t2 = t * t;
-			var t3 = t2 * t;
-			return c0 + c1 * t + c2 * t2 + c3 * t3;
-		}
+        public float Calc(float t) {
+            var t2 = t * t;
+            var t3 = t2 * t;
+            return c0 + c1 * t + c2 * t2 + c3 * t3;
+        }
 
-	}
+    }
 
-	public class CatmullRomCurve : Curve {
+    public class CatmullRomCurve : Curve {
 
-		CubicPoly px, py, pz;
+        CubicPoly px, py, pz;
 
-		public CatmullRomCurve(List<Vector3> points, bool closed = false) : base(points, closed) {
-			px = new CubicPoly();
-			py = new CubicPoly();
-			pz = new CubicPoly();
-		}
+        public CatmullRomCurve(List<Vector3> points, bool closed = false) : base(points, closed) {
+            px = new CubicPoly();
+            py = new CubicPoly();
+            pz = new CubicPoly();
+        }
 
-		public override Vector3 GetPoint(float t) {
-			var points = this.points;
-			var l = points.Count;
+        public override Vector3 GetPoint(float t) {
+            var points = this.points;
+            var l = points.Count;
 
-			var point = (l - (this.closed ? 0 : 1)) * t;
-			var intPoint = Mathf.FloorToInt(point);
-			var weight = point - intPoint;
+            var point = (l - (this.closed ? 0 : 1)) * t;
+            var intPoint = Mathf.FloorToInt(point);
+            var weight = point - intPoint;
 
-			if (this.closed) {
-				intPoint += intPoint > 0 ? 0 : (Mathf.FloorToInt(Mathf.Abs(intPoint) / points.Count) + 1) * points.Count;
-			} else if (weight == 0 && intPoint == l - 1) {
-				intPoint = l - 2;
-				weight = 1;
-			}
+            if (this.closed) {
+                intPoint += intPoint > 0 ? 0 : (Mathf.FloorToInt(Mathf.Abs(intPoint) / points.Count) + 1) * points.Count;
+            } else if (weight == 0 && intPoint == l - 1) {
+                intPoint = l - 2;
+                weight = 1;
+            }
 
-			Vector3 tmp, p0, p1, p2, p3; // 4 points
-			if (this.closed || intPoint > 0) {
-				p0 = points[(intPoint - 1) % l];
-			} else {
-				// extrapolate first point
-				tmp = (points[0] - points[1]) + points[0];
-				p0 = tmp;
-			}
+            Vector3 tmp, p0, p1, p2, p3; // 4 points
+            if (this.closed || intPoint > 0) {
+                p0 = points[(intPoint - 1) % l];
+            } else {
+                // extrapolate first point
+                tmp = (points[0] - points[1]) + points[0];
+                p0 = tmp;
+            }
 
-			p1 = points[intPoint % l];
-			p2 = points[(intPoint + 1) % l];
+            p1 = points[intPoint % l];
+            p2 = points[(intPoint + 1) % l];
 
-			if (this.closed || intPoint + 2 < l) {
-				p3 = points[(intPoint + 2) % l];
-			} else {
-				// extrapolate last point
-				tmp = (points[l - 1] - points[l - 2]) + points[l - 1];
-				p3 = tmp;
-			}
+            if (this.closed || intPoint + 2 < l) {
+                p3 = points[(intPoint + 2) % l];
+            } else {
+                // extrapolate last point
+                tmp = (points[l - 1] - points[l - 2]) + points[l - 1];
+                p3 = tmp;
+            }
 
-			const float tension = 0.5f;
-			px.InitCatmullRom(p0.x, p1.x, p2.x, p3.x, tension);
-			py.InitCatmullRom(p0.y, p1.y, p2.y, p3.y, tension);
-			pz.InitCatmullRom(p0.z, p1.z, p2.z, p3.z, tension);
+            const float tension = 0.5f;
+            px.InitCatmullRom(p0.x, p1.x, p2.x, p3.x, tension);
+            py.InitCatmullRom(p0.y, p1.y, p2.y, p3.y, tension);
+            pz.InitCatmullRom(p0.z, p1.z, p2.z, p3.z, tension);
 
-			return new Vector3(px.Calc(weight), py.Calc(weight), pz.Calc(weight));
-		}
+            return new Vector3(px.Calc(weight), py.Calc(weight), pz.Calc(weight));
+        }
 
 
-	}
-		
+    }
+
 }
 
--- Assets/Packages/Curves/Scripts/Curve.cs
+++ Assets/Packages/Curves/Scripts/Curve.cs
@@ -6,238 +6,228 @@ using UnityEngine;
 
 namespace Curve {
 
-	public abstract class Curve {
+    public abstract class Curve {
 
-		protected List<Vector3> points;
-		protected bool closed;
+        protected List<Vector3> points;
+        protected bool closed;
 
-		protected float[] cacheArcLengths;
-		bool needsUpdate;
+        protected float[] cacheArcLengths;
+        bool needsUpdate;
 
-		public Curve(List<Vector3> points, bool closed = false) {
-			this.points = points;
-			this.closed = closed;
-		}
+        public Curve(List<Vector3> points, bool closed = false) {
+            this.points = points;
+            this.closed = closed;
+        }
 
-		public abstract Vector3 GetPoint(float t);
+        public abstract Vector3 GetPoint(float t);
 
-		public virtual Vector3 GetTangent(float t) {
-			var delta = 0.001f;
-			var t1 = t - delta;
-			var t2 = t + delta;
+        public virtual Vector3 GetTangent(float t) {
+            var delta = 0.001f;
+            var t1 = t - delta;
+            var t2 = t + delta;
 
-			// Capping in case of danger
-			if (t1 < 0f) t1 = 0f;
-			if (t2 > 1f) t2 = 1f;
+            // Capping in case of danger
+            if (t1 < 0f) t1 = 0f;
+            if (t2 > 1f) t2 = 1f;
 
-			var pt1 = GetPoint(t1);
-			var pt2 = GetPoint(t2);
-			return (pt2 - pt1).normalized;
-		}
+            var pt1 = GetPoint(t1);
+            var pt2 = GetPoint(t2);
+            return (pt2 - pt1).normalized;
+        }
 
-		public Vector3 GetPointAt(float u) {
-			float t = GetUtoTmapping(u);
-			return GetPoint(t);
-		}
+        public Vector3 GetPointAt(float u) {
+            float t = GetUtoTmapping(u);
+            return GetPoint(t);
+        }
 
-		public Vector3 GetTangentAt(float u) {
-			float t = GetUtoTmapping(u);
-			return GetTangent(t);
-		}
+        public Vector3 GetTangentAt(float u) {
+            float t = GetUtoTmapping(u);
+            return GetTangent(t);
+        }
 
-		float[] GetLengths(int divisions = -1) {
-			if (divisions < 0) {
-				divisions = 200;
-			}
+        float[] GetLengths(int divisions = -1) {
+            if (divisions < 0) {
+                divisions = 200;
+            }
 
-			if (this.cacheArcLengths != null &&
-				(this.cacheArcLengths.Length == divisions + 1) &&
-				!this.needsUpdate) {
-				return this.cacheArcLengths;
-			}
+            if (this.cacheArcLengths != null &&
+                    (this.cacheArcLengths.Length == divisions + 1) &&
+                    !this.needsUpdate) {
+                return this.cacheArcLengths;
+            }
 
-			this.needsUpdate = false;
+            this.needsUpdate = false;
 
-			var cache = new float[divisions + 1];
-			Vector3 current, last = this.GetPoint(0f);
+            var cache = new float[divisions + 1];
+            Vector3 current, last = this.GetPoint(0f);
 
-			cache[0] = 0f;
+            cache[0] = 0f;
 
-			float sum = 0f;
-			for (int p = 1; p <= divisions; p ++ ) {
-				current = this.GetPoint(1f * p / divisions);
-				sum += Vector3.Distance(current, last);
-				cache[p] = sum;
-				last = current;
-			}
+            float sum = 0f;
+            for (int p = 1; p <= divisions; p ++ ) {
+                current = this.GetPoint(1f * p / divisions);
+                sum += Vector3.Distance(current, last);
+                cache[p] = sum;
+                last = current;
+            }
 
-			this.cacheArcLengths = cache;
-			return cache;
-		}
+            this.cacheArcLengths = cache;
+            return cache;
+        }
 
-		// Given u ( 0 .. 1 ), get a t to find p. This gives you points which are equidistant
-		protected float GetUtoTmapping(float u) {
-			var arcLengths = this.GetLengths();
+        // Given u ( 0 .. 1 ), get a t to find p. This gives you points which are equidistant
+        protected float GetUtoTmapping(float u) {
+            var arcLengths = this.GetLengths();
 
-			int i = 0, il = arcLengths.Length;
+            int i = 0, il = arcLengths.Length;
 
             // The targeted u distance value to get
-			float targetArcLength = u * arcLengths[il - 1];
+            float targetArcLength = u * arcLengths[il - 1];
 
-			// binary search for the index with largest value smaller than target u distance
+            // binary search for the index with largest value smaller than target u distance
+            int low = 0, high = il - 1;
+            float comparison;
 
-			int low = 0, high = il - 1;
-			float comparison;
+            while ( low <= high ) {
 
-			while ( low <= high ) {
+                i = Mathf.FloorToInt(low + (high - low) / 2f);
+                comparison = arcLengths[i] - targetArcLength;
 
-				i = Mathf.FloorToInt(low + (high - low) / 2f); // less likely to overflow, though probably not issue here, JS doesn't really have integers, all numbers are floats
+                if (comparison < 0f) {
+                    low = i + 1;
+                } else if (comparison > 0f) {
+                    high = i - 1;
+                } else {
+                    high = i;
+                    break;
+                }
 
-				comparison = arcLengths[i] - targetArcLength;
+            }
 
-				if (comparison < 0f) {
+            i = high;
 
-					low = i + 1;
+            if (Mathf.Approximately(arcLengths[i], targetArcLength)) {
 
-				} else if (comparison > 0f) {
+                return 1f * i / ( il - 1 );
 
-					high = i - 1;
+            }
 
-				} else {
+            // we could get finer grain at lengths, or use simple interpolation between two points
 
-					high = i;
-					break;
+            var lengthBefore = arcLengths[i];
+            var lengthAfter = arcLengths[i + 1];
 
-					// DONE
+            var segmentLength = lengthAfter - lengthBefore;
 
-				}
+            // determine where we are between the 'before' and 'after' points
 
-			}
+            var segmentFraction = ( targetArcLength - lengthBefore ) / segmentLength;
 
-			i = high;
+            // add that fractional amount to t
+            var t = 1f * (i + segmentFraction) / (il - 1);
 
-			if (Mathf.Approximately(arcLengths[i], targetArcLength)) {
+            return t;
+        }
 
-				return 1f * i / ( il - 1 );
+        public List<FrenetFrame> ComputeFrenetFrames (int segments, bool closed) {
+            var normal = new Vector3();
 
-			}
+            var tangents = new Vector3[segments + 1];
+            var normals = new Vector3[segments + 1];
+            var binormals = new Vector3[segments + 1];
 
-			// we could get finer grain at lengths, or use simple interpolation between two points
+            var vec = new Vector3();
+            var mat = new Matrix4x4();
 
-			var lengthBefore = arcLengths[i];
-			var lengthAfter = arcLengths[i + 1];
+            float u, theta;
 
-			var segmentLength = lengthAfter - lengthBefore;
+            // compute the tangent vectors for each segment on the curve
+            for (int i = 0; i <= segments; i++) {
+                u = (1f * i) / segments;
+                tangents[i] = GetTangentAt(u).normalized;
+            }
 
-			// determine where we are between the 'before' and 'after' points
+            // select an initial normal vector perpendicular to the first tangent vector,
+            // and in the direction of the minimum tangent xyz component
 
-			var segmentFraction = ( targetArcLength - lengthBefore ) / segmentLength;
+            normals[0] = new Vector3();
+            binormals[0] = new Vector3();
 
-			// add that fractional amount to t
-			var t = 1f * (i + segmentFraction) / (il - 1);
+            var min = float.MaxValue;
+            var tx = Mathf.Abs(tangents[0].x);
+            var ty = Mathf.Abs(tangents[0].y);
+            var tz = Mathf.Abs(tangents[0].z);
+            if (tx <= min) {
+                min = tx;
+                normal.Set(1, 0, 0);
+            }
+            if (ty <= min) {
+                min = ty;
+                normal.Set(0, 1, 0);
+            }
+            if (tz <= min) {
+                normal.Set(0, 0, 1);
+            }
 
-			return t;
-		}
+            vec = Vector3.Cross(tangents[0], normal).normalized;
 
-		public List<FrenetFrame> ComputeFrenetFrames (int segments, bool closed) {
-			var normal = new Vector3();
+            normals[0] = Vector3.Cross(tangents[0], vec);
+            binormals[0] = Vector3.Cross(tangents[0], normals[0]);
 
-			var tangents = new Vector3[segments + 1];
-			var normals = new Vector3[segments + 1];
-			var binormals = new Vector3[segments + 1];
+            // compute the slowly-varying normal and binormal vectors for each segment on the curve
 
-			var vec = new Vector3();
-			var mat = new Matrix4x4();
+            for (int i = 1; i <= segments; i++) {
+                // copy previous
+                normals[i] = normals[i - 1];
+                binormals[i] = binormals[i - 1];
 
-			float u, theta;
+                // Rotation axis
+                vec = Vector3.Cross(tangents[i - 1], tangents[i]);
 
-			// compute the tangent vectors for each segment on the curve
-			for (int i = 0; i <= segments; i++) {
-				u = (1f * i) / segments;
-				tangents[i] = GetTangentAt(u).normalized;
-			}
+                if (vec.magnitude > float.Epsilon) {
+                    vec.Normalize();
 
-			// select an initial normal vector perpendicular to the first tangent vector,
-			// and in the direction of the minimum tangent xyz component
+                    float dot = Vector3.Dot(tangents[i - 1], tangents[i]);
 
-			normals[0] = new Vector3();
-			binormals[0] = new Vector3();
+                    // clamp for floating pt errors
+                    theta = Mathf.Acos(Mathf.Clamp(dot, -1f, 1f));
 
-			var min = float.MaxValue;
-			var tx = Mathf.Abs(tangents[0].x);
-			var ty = Mathf.Abs(tangents[0].y);
-			var tz = Mathf.Abs(tangents[0].z);
-			if (tx <= min) {
-				min = tx;
-				normal.Set(1, 0, 0);
-			}
-			if (ty <= min) {
-				min = ty;
-				normal.Set(0, 1, 0);
-			}
-			if (tz <= min) {
-				normal.Set(0, 0, 1);
-			}
+                    normals[i] = Quaternion.AngleAxis(theta * Mathf.Rad2Deg, vec) * normals[i];
+                }
 
-			vec = Vector3.Cross(tangents[0], normal).normalized;
+                binormals[i] = Vector3.Cross(tangents[i], normals[i]).normalized;
+            }
 
-			normals[0] = Vector3.Cross(tangents[0], vec);
-			binormals[0] = Vector3.Cross(tangents[0], normals[0]);
+            // if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
 
-			// compute the slowly-varying normal and binormal vectors for each segment on the curve
+            if (closed) {
+                theta = Mathf.Acos(Mathf.Clamp(Vector3.Dot(normals[0], normals[segments]), -1f, 1f));
+                theta /= segments;
 
-			for (int i = 1; i <= segments; i++) {
-				// copy previous
-				normals[i] = normals[i - 1];
-				binormals[i] = binormals[i - 1];
+                if (Vector3.Dot(tangents[0], Vector3.Cross(normals[0], normals[segments])) > 0f) {
+                    theta = - theta;
+                }
 
-				// Rotation axis
-				vec = Vector3.Cross(tangents[i - 1], tangents[i]);
+                for (int i = 1; i <= segments; i++) {
+                    normals[i] = (Quaternion.AngleAxis(Mathf.Deg2Rad * theta * i, tangents[i]) * normals[i]);
+                    binormals[i] = Vector3.Cross(tangents[i], normals[i]);
+                }
 
-				if (vec.magnitude > float.Epsilon) {
-					vec.Normalize();
+            }
 
-					float dot = Vector3.Dot(tangents[i - 1], tangents[i]);
+            var frames = new List<FrenetFrame>();
 
-					// clamp for floating pt errors
-					theta = Mathf.Acos(Mathf.Clamp(dot, -1f, 1f));
+            int n = tangents.Length;
+            for(int i = 0; i < n; i++) {
+                var frame = new FrenetFrame(tangents[i], normals[i], binormals[i]);
+                frames.Add(frame);
+            }
 
-					normals[i] = Quaternion.AngleAxis(theta * Mathf.Rad2Deg, vec) * normals[i];
-				}
-
-				binormals[i] = Vector3.Cross(tangents[i], normals[i]).normalized;
-			}
-
-			// if the curve is closed, postprocess the vectors so the first and last normal vectors are the same
-
-			if (closed) {
-				theta = Mathf.Acos(Mathf.Clamp(Vector3.Dot(normals[0], normals[segments]), -1f, 1f));
-				theta /= segments;
-
-				if (Vector3.Dot(tangents[0], Vector3.Cross(normals[0], normals[segments])) > 0f) {
-					theta = - theta;
-				}
-
-				for (int i = 1; i <= segments; i++) {
-					normals[i] = (Quaternion.AngleAxis(Mathf.Deg2Rad * theta * i, tangents[i]) * normals[i]);
-					binormals[i] = Vector3.Cross(tangents[i], normals[i]);
-				}
-
-			}
-
-			var frames = new List<FrenetFrame>();
-
-			int n = tangents.Length;
-			for(int i = 0; i < n; i++) {
-				var frame = new FrenetFrame(tangents[i], normals[i], binormals[i]);
-				frames.Add(frame);
-			}
-
-			return frames;
-		}
+            return frames;
+        }
 
 
-	}
-		
+    }
+
 }
 
